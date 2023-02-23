d853400fa10344cff84c141911bc5c1ce78c7956
Leonardo <leo.girardi97@hotmail.com>
2023-02-23 17:59:58 -0300

    updated home screen

---
+++ mobile/src/components/PendencyCard.tsx
@@ -0,0 +1,47 @@
+// Native
+import { useState } from 'react'
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { Heading, HStack, Icon, Skeleton, Text, VStack } from 'native-base'
+// Assets
+import { Entypo } from '@expo/vector-icons'
+import WarningSvg from '@assets/warning.svg'
+
+type Props = TouchableOpacityProps
+
+export function PendencyCard({ ...rest }: Props) {
+  const [isLoading, setIsLoading] = useState(false)
+  return (
+    <TouchableOpacity {...rest}>
+      {isLoading ? (
+        <Skeleton
+          h={16}
+          rounded="md"
+          startColor="gray.600"
+          endColor="gray.400"
+          mb={3}
+        />
+      ) : (
+        <HStack
+          bg="gray.500"
+          alignItems="center"
+          p={2}
+          pl={5}
+          pr={5}
+          rounded="md"
+          mb={3}
+        >
+          <WarningSvg width={24} height={24} />
+          <VStack flex={1} ml={4}>
+            <Heading fontSize="lg" color="gray.200">
+              Visita #01
+            </Heading>
+            <Text fontSize="sm" color="gray.300" mt={1} numberOfLines={1}>
+              Cliente
+            </Text>
+          </VStack>
+          <Icon as={Entypo} name="chevron-thin-right" color="gray.300" />
+        </HStack>
+      )}
+    </TouchableOpacity>
+  )
+}
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,9 +1,46 @@
-import { Center, Text } from 'native-base'
+// Native
+import { useState } from 'react'
+import { FlatList, Heading, HStack, Text, VStack } from 'native-base'
+// Components
+import { HomeHeader } from '@components/Header'
+import { EmptyList } from '@components/EmptyList'
+import { PendencyCard } from '@components/PendencyCard'
+import { Button } from '@components/Button'
+import { NavHome } from '@components/NavHome'
 
 export function Home() {
+  const [pendencies, setPendencies] = useState([''])
   return (
-    <Center flex={1}>
-      <Text>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <HStack p={8}>
+        <NavHome title="registro da visita" />
+        <NavHome title="planejamento do dia" />
+      </HStack>
+      <VStack flex={1} px={8} mt={4}>
+        <HStack justifyContent="space-between">
+          <Heading color="gray.200" fontSize="md" mb={4}>
+            Pendentes
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            {pendencies.length}
+          </Text>
+        </HStack>
+        <FlatList
+          data={pendencies}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <PendencyCard />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 10 }}
+          contentContainerStyle={
+            pendencies.length === 0 && { flex: 1, justifyContent: 'center' }
+          }
+          ListEmptyComponent={() => (
+            <EmptyList title="Não há cadastros pendentes" />
+          )}
+        />
+      </VStack>
+    </VStack>
   )
 }
