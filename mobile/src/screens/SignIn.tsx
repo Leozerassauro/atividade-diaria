ecd5cc86ce07f0f29dc825bde825b08fde848a50
Leonardo <leo.girardi97@hotmail.com>
2023-02-23 16:47:33 -0300

    added background image to SignIn screen

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,40 +1,38 @@
 // Native
-import { Heading, HStack, Icon, VStack } from 'native-base'
+import { VStack, Image } from 'native-base'
 // Components
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 // Assets
-import { Entypo } from '@expo/vector-icons'
+import Logo from '@assets/logo.png'
+import BackgroundImg from '@assets/background.png'
 
 export function SignIn() {
   return (
-    <VStack
-      flex={1}
-      bg="gray.700"
-      justifyContent="center"
-      alignItems="center"
-      p={10}
-    >
-      <HStack mb={20}>
-        <Icon as={Entypo} name="pin" color="green.700" size={6} />
-        <Heading color="gray.200" ml={2} fontFamily="heading">
-          Gestão de visita
-        </Heading>
-      </HStack>
-      <Input
-        bg="gray.600"
-        placeholder="Digite o usuário"
-        mb={8}
-        autoCapitalize="none"
+    <VStack flex={1} bg="gray.700" p={10}>
+      <Image
+        source={BackgroundImg}
+        alt="Imagem com produtos da empresa LNF"
+        resizeMode="contain"
+        position="absolute"
       />
-      <Input
-        bg="gray.600"
-        placeholder="Digite a senha"
-        mb={16}
-        secureTextEntry
-        returnKeyType="send"
-      />
-      <Button title="Entrar" />
+      <VStack flex={1} justifyContent="center" alignItems="center">
+        <Image source={Logo} alt="Imagem do logo da empresa LNF" mb={10} />
+        <Input
+          bg="gray.700"
+          placeholder="Digite o usuário"
+          mb={8}
+          autoCapitalize="none"
+        />
+        <Input
+          bg="gray.700"
+          placeholder="Digite a senha"
+          mb={16}
+          secureTextEntry
+          returnKeyType="send"
+        />
+        <Button title="Entrar" />
+      </VStack>
     </VStack>
   )
 }
